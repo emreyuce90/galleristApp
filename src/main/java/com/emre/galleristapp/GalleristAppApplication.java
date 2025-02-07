5cdaf0d332f1ea9f00faa5f7fdc7c840dda7d4fb
Emre Yüce <emre.yuce@simetri8.com>
2025-02-07 17:43:57 +0300

    initial commit

---
+++ src/main/java/com/emre/galleristapp/GalleristAppApplication.java
@@ -0,0 +1,13 @@
+package com.emre.galleristapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleristAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalleristAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emre/galleristapp/GalleristAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emre.galleristapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleristAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
